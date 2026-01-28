04ea3689c0bf317304942254f1912dc8593d996d
SevenLawless <7lawless25@gmail.com>
2026-01-28 16:47:21 +0100

    uh huh

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,15 @@
-function checkAge(userAge) {
-  debugger;  // Code will pause herex`x
-  
-  if (userAge >= 18) {
-    return "Adult";
-  }
-  return "Minor";
+function l3yba(myAge) {
+    const momAge = myAge * 2;
+    console.log(`My mom's age is ${momAge}`);
+    console.log(`My dad's age is ${momAge * 1.2}`);
 }
+
+l3yba(22);
+
+
+
+function l3yba2(myAge) {
+    const momAge = myAge * 2;
+    return momAge;
+}
+console.log(l3yba2(22));
